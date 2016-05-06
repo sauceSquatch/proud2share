ec8c9ddf7547b2e6208d957645d7677415090b0b
Brad Tippett <brad@bradtippett.com>
2016-05-06 16:07:06 -0500

    Ditch the shell script for fixing Keystone dependencies

--- scripts/fixKeystoneDependencies.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-[ ! -d ./node_modules/keystone/node_modules/babel-core ] && npm install --prefix ./node_modules/keystone babel-core@5.8.25
-[ ! -d ./node_modules/keystone/node_modules/babel-plugin-object-assign ] && npm install --prefix ./node_modules/keystone babel-plugin-object-assign@1.2.1
-[ ! -d ./node_modules/keystone/node_modules/babelify ] && npm install --prefix ./node_modules/keystone babelify@6.3.0
